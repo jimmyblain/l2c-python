82ecef017605d6df93a2d705dc0762c1b8fc7f5b
jimmyblain <glajummyblain@gmail.com>
2024-11-12 14:55:10 -0500

    more hello world code and folder rename

--- back_2_basics/hello_World.py
+++ back_2_basics/hello_World.py
@@ -1,6 +1,7 @@
-
 # Declaring the "name" variable, sending input from user.
 name = input('Please enter your name: ')
 
+print() #blank line
+
 # Printing static text, followed by "name" variable.
-print('Hello ', name)
+print('Hello', name, '\nHave a good day.') # \n to make a new line.
