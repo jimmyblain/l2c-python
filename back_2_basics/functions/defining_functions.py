045e7221e1b453e1c70614bac196ab8400ec41e5
jimmyblain <glajummyblain@gmail.com>
2024-12-02 18:26:31 -0500

    Cleaned up syntax and styling

--- back_2_basics/functions/defining_functions.py
+++ back_2_basics/functions/defining_functions.py
@@ -18,7 +18,7 @@ favorite_book('the bible')
 #8-3, 4
 
 #Define default values for parameters, which the variables will be set to is no arguement is passed.
-def make_shirt (size = 'large', text = 'I love Python'):
+def make_shirt (size='large', text='I love Python'):
     """Print a message about shirt size and text on shirt."""
     print(f"\nThe size of the shirt is a(n) {size.upper()}.")
     print(f"The shirt will read '{text}'.")
@@ -27,7 +27,7 @@ def make_shirt (size = 'large', text = 'I love Python'):
 make_shirt('Xxl', 'I like turtles')
 
 #Passing keyword arguements (order does not matter).
-make_shirt(text = 'I love programming', size = 'small')
+make_shirt(text='I love programming', size='small')
 
 #Function will use default values when no parameters are passed.
 make_shirt()
@@ -35,7 +35,7 @@ make_shirt(text = "It's really late right now")
 
 #8-7
 
-def describe_city(city_name, city_country = 'America'):
+def describe_city(city_name, city_country='America'):
     print(f"\n{city_name.title()} is located in {city_country}.")
 
 describe_city('Brooklyn')
