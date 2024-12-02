fdcb0f011cd61c464dddf0efa2bb79bfa0bf889d
jimmyblain <glajummyblain@gmail.com>
2024-12-02 18:27:49 -0500

    moved functions to a separate module, then showed various ways to import that module

--- back_2_basics/functions/functions_with_lists.py
+++ back_2_basics/functions/functions_with_lists.py
@@ -1,28 +1,28 @@
 #8-9
+#Functions have been moved to messages_functions.py module, which we import below:
+import messages_functions
+
+#Importing specific functions from module
+from messages_functions import display_messages, send_messages
+#Importing all from module (not recommended)
+from messages_functions import *
+#Importing function name as alias from module (See line 25)
+from messages_functions import send_messages as sm
+#Importing module as alias to clean up code (See line 35)
+import messages_functions as mf 
 
 short_messages = ['Hello Jimmy.', 'Hope you have had a good day.', "I'll see you later!"]
 
-def display_messages(messages):
-    """Pass a list of messages, print each message in the list"""
-    while messages:
-        current_message = messages.pop()
-        print(current_message)
-
-display_messages(short_messages)
+#Using full module and function name
+messages_functions.display_messages(short_messages)
 
 #8-10
 short_messages = ['Hello Jimmy.', 'Hope you have had a good day.', "I'll see you later!"]
 sent_messages = []
 
-def send_messages(message_queue, messages_sent):
-    """Pass a list of messages, and an empty list. As messages are sent, move them to the empty list"""
-    while message_queue:
-        current_message = message_queue.pop()
-        print(current_message)
-        messages_sent.append(current_message)
-
 #Call function, checks that the lists move their values properly
-send_messages(short_messages, sent_messages)
+#Using alias name of function imported from module
+sm(short_messages, sent_messages)
 print(short_messages)
 print(sent_messages)
 
@@ -31,6 +31,7 @@ short_messages = ['Hello Jimmy.', 'Hope you have had a good day.', "I'll see you
 sent_messages = []
 
 #Pass a COPY of the list, so original list remains unchanged
-send_messages(short_messages[:], sent_messages)
+#Using alias name for module
+mf.send_messages(short_messages[:], sent_messages)
 print(short_messages)
 print(sent_messages)
