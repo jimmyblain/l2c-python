93d3ca95df3ddb2965f0a27c897bdc4f6729393d
jimmyblain <glajummyblain@gmail.com>
2024-12-08 17:21:25 -0500

    moved privileges and admin class to separate module

--- back_2_basics/007_classes/user_class.py
+++ back_2_basics/007_classes/user_class.py
@@ -1,4 +1,4 @@
-#9-3
+# 9-3, 5
 
 class User:
     '''Represent a user of a common system/service'''
@@ -11,6 +11,7 @@ class User:
         self.age = age
         self.gender = gender
         self.is_active = is_active
+        self.login_attempts = 0
 
     def describe_user(self):
         '''Lists out attributes of the User object'''
@@ -27,6 +28,36 @@ class User:
         else:
             print("Your account is disabled.")
 
+    def increment_login_attempts(self, login_attempt):
+        '''Increment the login attempt by 1'''
+        self.login_attempts += login_attempt
+
+    def reset_login_attempts(self):
+        '''Reset login attempts to 0'''
+        self.login_attempts = 0
+    
+    def get_login_attempts(self):
+        '''Print login attempts for given object'''
+        print(f"\n{self.first_name.title()} has {self.login_attempts} login attempts.")
+
+# 9-8
+class Privileges:
+    '''Contains available privileges for an admin user'''
+    def __init__(self, privileges = ['can add post', 'can delete posts', 'can ban user']):
+        self.privileges = privileges
+        
+    def show_privileges(self):
+        for privilege in self.privileges:
+            print(privilege.title())
+# 9-7
+class Admin(User):
+    '''Represent an administrator user object with special permissions'''
+    def __init__(self, first_name, last_name, age, gender, is_active=True):
+        super().__init__(first_name, last_name, age, gender, is_active)
+
+        # Creating an Privileges instance within the Admin class as an attribute.
+        self.privileges = Privileges()
+
 
 baromega = User("Jimmy", "Blain", 32, "Male", False)
 lishspeaks = User("Alicia", "Blain", 36, "Female")
@@ -35,4 +66,19 @@ lishspeaks.describe_user()
 baromega.greet_user()
 lishspeaks.greet_user()
 
-        
+lishspeaks.get_login_attempts()
+
+lishspeaks.increment_login_attempts(1)
+lishspeaks.increment_login_attempts(1)
+lishspeaks.increment_login_attempts(1)       
+lishspeaks.increment_login_attempts(1)
+lishspeaks.get_login_attempts()
+
+lishspeaks.reset_login_attempts()
+lishspeaks.get_login_attempts()
+
+sudo = Admin('John', 'Doe', 63, 'Non-Binary')
+sudo.describe_user()
+
+# Calling a method from the privilege class on a privilege object attribute within the Admin class
+sudo.privileges.show_privileges()
