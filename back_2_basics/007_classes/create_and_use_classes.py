6b6f48b13a0ab39034dbd314cb6744f2492b0fb8
jimmyblain <glajummyblain@gmail.com>
2024-12-08 17:22:08 -0500

    created IceCreamStand child class to Restaurant parent

--- back_2_basics/007_classes/create_and_use_classes.py
+++ back_2_basics/007_classes/create_and_use_classes.py
@@ -1,21 +1,21 @@
-#9-1, 2
+# 9-1, 2, 4
 
-#Class names should always be capitalized.
+# Class names should always be capitalized.
 class Restaurant:
     '''Representation of a restaurant'''
 
-    #Required method, runs every time a new object is initialized
-    #Self is a required argument, everything else is dependent on your class
+    # Required method, runs every time a new object is initialized
+    # Self is a required argument, everything else is dependent on your class
     def __init__(self, name, cuisine):
-        """Initialize attributes to describe a restaurant"""
+        '''Initialize attributes to describe a restaurant'''
         self.name = name
         self.cuisine = cuisine
         self.number_served = 0
 
     def describe_restaurant(self):
         '''Print name and cuisine of restaurant'''
-        print(f"The name of this restaurant is {self.name.title()}.")
-        print(f"They serve {self.cuisine.title()} food here.")
+        print(f"\nThe name of this restaurant is {self.name.title()}.")
+        print(f"They serve {self.cuisine.title()} here.")
 
     def open_restaurant(self):
         '''Print that the restaurant in open'''
@@ -30,10 +30,27 @@ class Restaurant:
         self.number_served += served
 
     def get_number_served(self):
+        '''Print number of patrons served for passed object'''
         print(f"\n{self.name.title()} has served {self.number_served} customers.")
 
-    
+# 9-6
+# Creating a child class to the parent class Restaurant
+# Class names are written in CamelCase
+class IceCreamStand (Restaurant):
+    '''Modeling an ice cream stand, which is a type of restraurant'''
+    def __init__(self, name, cuisine):
+        '''
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to the child class
+        '''
+        #super() is a special method that calls a method from the parent class
+        super().__init__(name, cuisine)
+        self.flavors = ['vanilla', 'chocolate', 'strawberry']
 
+    def get_flavors(self):
+        '''Returns the list of flavors'''
+        flavor_list = self.flavors[:]
+        return flavor_list
     
 
 
@@ -50,3 +67,7 @@ burger_king.set_number_served(5)
 burger_king.get_number_served()
 burger_king.increment_number_served(10)
 burger_king.get_number_served()
+
+coldstone = IceCreamStand('Cold Stone', 'Dessert')
+coldstone.describe_restaurant()
+print(coldstone.get_flavors())
