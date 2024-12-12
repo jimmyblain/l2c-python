83928883a1b90449b477d3d7df36125388e1fd4d
jimmyblain <glajummyblain@gmail.com>
2024-12-12 08:13:35 -0500

    simple txt file and a program to read its contents

--- back_2_basics/008_files_and_exceptions/reading_files.py
+++ back_2_basics/008_files_and_exceptions/reading_files.py
@@ -1,7 +1,21 @@
 #10-1
-
 from pathlib import Path
 
-file_path = Path('learning_python.txt')
-contents = file_path.read_text()
-print(contents)
+#Need to use relative path starting at project folder.
+file_path = Path('back_2_basics/008_files_and_exceptions/learning_python.txt')
+
+# Method chaining - runs methods from left to right
+# This will strip the empty character/line at the end of the txt file
+contents = file_path.read_text().rstrip()
+print(f"{contents}\n")
+
+# splitlines() returns a list of strings, line by line
+for line in contents.splitlines():
+    print(line)
+
+# 10 -2
+# replace() returns the line with the replaced text
+message = ''
+for old_line in contents.splitlines():
+    message = old_line.replace('Python', 'C++')
+    print(message)
