387f9eaa4fc57aae3600e1d347b70b10140043a5
jimmyblain <glajummyblain@gmail.com>
2024-12-12 22:17:48 -0500

    program to count the appearances of a word in a large piece of text

---
+++ back_2_basics/008_files_and_exceptions/analyze_large_texts.py
@@ -0,0 +1,10 @@
+# 10-10
+from pathlib import Path
+
+file_path = Path('back_2_basics/008_files_and_exceptions/prideandprejudice.txt')
+book_words = file_path.read_text()
+
+# Convert words to lower case, count how many times "the" appears
+# Include the space after so you don't count words like "then" and "there"
+the_count = book_words.lower().count('the ')
+print(f"The word 'the' appears {the_count} times.")
